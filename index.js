a2d1a38e3264692ae6de090f0433e45465f9bacb
anujyadavofficial <anuj.yadav.official.84@gmail.com>
2022-06-19 01:16:28 +0530

    1 file modified

--- index.js
+++ index.js
@@ -152,3 +152,35 @@ if (pnrBox) {
 if (nameBox) {
   nameBox.value = 'Kumar Gaurav';
 }
+
+var VM = {
+  Specs: [
+    {
+      title: 'Only Two Input Elements',
+      description: 'SG uses this approach.',
+      index: 0,
+    },
+    {
+      title: 'Many Input Elements',
+      description: 'Vistara uses this',
+      index: 1,
+    },
+  ],
+  Rules: [{ id: 'Found exact two', confidence: 0.5 }],
+  Renderer: {
+    renderSpecs: function () {
+      var template = $('#tmpl-spec').html();
+      var templateScript = Handlebars.compile(template);
+
+      Specs.forEach(function (spec) {
+        var html = templateScript(spec);
+        // Insert the HTML code into the page
+        $('.list-group').append(html);
+      });
+    },
+  },
+};
+
+setTimeout(function () {
+  VM.Renderer.renderSpecs();
+}, 500);
