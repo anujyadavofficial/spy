50363a2066b636afcbb3dbc8f81c39988c824d61
anujyadavofficial <anuj.yadav.official.84@gmail.com>
2022-06-19 00:51:20 +0530

    Lost some changes, doing an intermittent one

---
+++ view-model.js
@@ -0,0 +1,14 @@
+var VM = {
+  Specs: [
+    {title: 'Only Two Input Elements', description: 'SG uses this approach.', index: 0},
+    {title: 'Many Input Elements', description: 'Vistara uses this', index: 1}
+  ],
+  Rules: [
+    {id : 'Found exact two', confidence: 0.5}
+  ],
+  Renderer:  {
+    renderSpecs: function() {
+      var template = $('#tmpl-')
+    }
+  }
+}
